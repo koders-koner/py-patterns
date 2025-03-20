83c7707e6ce5c3bc6f48bb5b5d7d272d656a5717
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2025-03-20 23:22:24 +0200

    Sleep patterns are fascinating.

---
+++ print_patterns.py
@@ -0,0 +1,148 @@
+def pattern_1(n):
+    """Simple star line
+    For n=3:
+    ***
+    """
+    # TODO: Implement simple horizontal line of n stars
+    pass
+
+def pattern_2(n):
+    """Vertical star line
+    For n=3:
+    *
+    *
+    *
+    """
+    # TODO: Implement vertical line of n stars
+    pass
+
+def pattern_3(n):
+    """Square of stars
+    For n=3:
+    ***
+    ***
+    ***
+    """
+    # TODO: Implement n x n square of stars
+    pass
+
+def pattern_4(n):
+    """Right triangle
+    For n=3:
+    *
+    **
+    ***
+    """
+    # TODO: Implement right triangle with n rows
+    pass
+
+def pattern_5(n):
+    """Reverse right triangle
+    For n=3:
+    ***
+    **
+    *
+    """
+    # TODO: Implement reverse right triangle with n rows
+    pass
+
+def pattern_6(n):
+    """Square border
+    For n=3:
+    ***
+    * *
+    ***
+    """
+    # TODO: Implement hollow square with n size
+    pass
+
+def pattern_7(n):
+    """Centered triangle
+    For n=3:
+      *
+     ***
+    *****
+    """
+    # TODO: Implement centered triangle with n rows
+    pass
+
+def pattern_8(n):
+    """Number line
+    For n=3:
+    1 2 3
+    """
+    # TODO: Implement horizontal line of numbers 1 to n
+    pass
+
+def pattern_9(n):
+    """Checkerboard
+    For n=3:
+    * * 
+     * *
+    * * 
+    """
+    # TODO: Implement alternating checkerboard pattern
+    pass
+
+def pattern_10(n):
+    """Diamond border
+    For n=3:
+      *
+     * *
+    *   *
+     * *
+      *
+    """
+    # TODO: Implement hollow diamond with n height for half
+    pass
+
+def pattern_11(n):
+    """Number triangle
+    For n=3:
+      1
+     1 2
+    1 2 3
+    """
+    # TODO: Implement number triangle with n rows
+    pass
+
+def pattern_12(n):
+    """X pattern
+    For n=3:
+    *   *
+      *  
+    *   *
+    """
+    # TODO: Implement X pattern with n size
+    pass
+
+def pattern_13(n):
+    """Staircase
+    For n=3:
+    ***
+     **
+      *
+    """
+    # TODO: Implement right-aligned staircase with n rows
+    pass
+
+def pattern_14(n):
+    """Wave pattern
+    For n=3:
+    * * *
+     * * *
+    * * *
+    """
+    # TODO: Implement wave pattern with n columns
+    pass
+
+def pattern_15(n):
+    """Frame pattern
+    For n=4:
+    +--+
+    |  |
+    |  |
+    +--+
+    """
+    # TODO: Implement frame pattern with n size
+    pass
